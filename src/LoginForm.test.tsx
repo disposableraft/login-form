cb93055862c90d9c4e477723aa0497f2513a6bd6
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-08-19 14:21:51 -0700

    Test LoginForm with and without values

--- src/LoginForm.test.tsx
+++ src/LoginForm.test.tsx
@@ -1,5 +1,21 @@
-import React from 'react'
-import LoginForm from './LoginForm'
-import {render, fireEvent} from '@testing-library/react'
+import React from "react";
+import LoginForm from "./LoginForm";
+import { render, fireEvent } from "@testing-library/react";
 
-test.todo('make a test or get out!')
+describe("LoginForm", () => {
+  test("Matches snapshot without values", () => {
+    const { container } = render(<LoginForm />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test("Matches snapshot with values", async () => {
+    const { findByTestId, getByLabelText } = render(<LoginForm />);
+    const fields = ["First Name", "Last Name", "Email", "Password"];
+    fields.forEach((name) => {
+      const input = getByLabelText(name);
+      fireEvent.change(input, { target: { value: "May you be happy" } });
+    });
+    const form = await findByTestId('form-login')
+    expect(form).toMatchSnapshot();
+  });
+});
